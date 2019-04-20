f5f39e09e0eaaf54e3a426e00cba2ea3a254aeb4
dio-perkasa <49556798+dio-perkasa@users.noreply.github.com>
2019-04-20 20:48:07 +0700

    Add files via upload

---
+++ 5180711108(modulus).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace::std;
+int mds(int nilai,int a){
+    if
+        (a==0)
+        {return (nilai%a);}
+    else if
+        (a==nilai)
+        {return (0);}
+    else
+        {return (nilai%a);}
+    }
+
+
+main(){
+    int n1,n2;
+    cout<<"input bilangan 1: ";cin>>n1;
+    cout<<"input bilangan 2: ";cin>>n2;
+    cout<<" Maka hasil = "<<mds(n1,n2);
+    }
